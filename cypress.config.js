f2f2b2fc50b95690ace88d4add405b74e3455d78
Sudhakar <sudhakar.sub@gmail.com>
2023-07-13 10:23:09 +0530

    minor modifications

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,19 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
 
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('file:preprocessor',cucumber())
     },
-    specPattern:"cypress/e2e/**/*.feature"
+    specPattern:"cypress/e2e/**/loginPage.feature"
+   
    
   },
   env: {
-    url: 'https://test-vw-1--release-gz7y0.docker2.motocommerce.ca/en/search',
+    url: 'https://test-vw-1--release-gz7y0.docker2.motocommerce.ca/',
     "grepFilterSpecs": true,
     "grepOmitFiltered": true
     
--- cypress/e2e/loginPage/loginPage.js
+++ cypress/e2e/loginPage/loginPage.js
@@ -1,6 +1,12 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
+import data from '../../fixtures/example.json'
 
 Given("I am on empty home page", () => {
     cy.visit(Cypress.env('url'));
-
+    //console.log(data);
+    //cy.log("Checking the data in JSON File",data);
+    cy.log("Name of the JOSON file",data.name);
+    cy.log("Body of the JOSON file",data.body);
+    cy.log("EMAIL of the JOSON file",data.email);
+   
 });
